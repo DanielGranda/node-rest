17cc50a9ba9d9d1e96ffbea1867477195201022f
PressUpApp <grandadaniel1987@gmail.com>
2019-12-19 14:29:48 -0500

    Variable Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urleDB;
 if (process.env.NODE_ENV === 'dev') {
     urleDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urleDB = 'mongodb://strider:UWFOSbqoLOyAOpmZ@cluster0-shard-00-00-reak0.mongodb.net:27017,cluster0-shard-00-01-reak0.mongodb.net:27017,cluster0-shard-00-02-reak0.mongodb.net:27017/cafe?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin'
+    urleDB = process.env.MONGO_URI;
 }
 
 
